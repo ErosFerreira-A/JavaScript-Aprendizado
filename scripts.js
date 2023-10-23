584a32b4027d0e5cd33e3982a186a2c1e4fe959a
ErosFerreira-A <erosferreira00@gmail.com>
2023-10-23 07:03:26 -0300

    Informações adicionais

---
+++ scripts.js
@@ -0,0 +1,93 @@
+//camelCase para escrita de variável, sendo que as mesmas são caseSensitive
+// Tipagem no JS não é alta e é dinâmica
+// Não existe diferenciação em JS, exemplo : number(int e float já está dentro desse conjunto)
+
+/*let nome = "Eros";
+let idade = 22;
+let altura = 1.80;
+
+
+console.log(nome);
+console.log(idade);
+console.log(altura);
+
+let pessoa = {
+    nome: 'Zeus',
+    idade: 22,
+    sobreNome: 'Ferreira'
+    
+};
+
+console.log(pessoa);
+
+// Arrays
+
+let familia = [22, 1, 22];
+console.log(familia.flatMap);
+
+
+
+// função
+let mudaId = nome + ',' + ' ' + idade; 
+function mudaIdentidade(nome, idade){
+    mudaId = nome + ' ' + idade;
+};
+
+console.log(mudaId);
+mudaIdentidade("Victor,", " 30");
+console.log(mudaId);
+
+
+function MultiplicarIdade(idade){
+    return idade* 10;
+}
+
+let resultado = MultiplicarIdade(22);
+document.write(`Você tem ${resultado}, velho demais !!!]`);
+
+let idade = 22;
+let tipo = idade === 22 ? 'premiu' : 'comum';
+document.write(tipo);
+
+//______________________
+
+*/
+
+let temDinheiro = true;
+let temTempo = true;
+let estabilidade = temTempo || temTempo;
+document.write('Você tem boas condições ', estabilidade);
+
+let hater = !estabilidade;
+document.write('Você é pobre, ', hater);
+
+// Outro código, testando IF e ELSE
+let comida = 'Gostosa';
+
+if (comida === 'Gostosa') {
+    console.log('Muito boa a comida, melhor restaurante !!!');
+} else {
+    console.log('Comida ruim, nunca mais volto aqui!!!')
+}
+
+// switch case
+
+let saude; // saudável, pouca saúde, grave
+saude = 'grave';
+
+switch(saude){
+    case'saudável':
+    console.log('Aprovado');
+    break;
+
+    case'pouca saúde':
+    console.log('Em análise');
+    break;
+
+    case'grave':
+    console.log('Reprovado');
+    break
+
+    default:
+    console.log('Comando não reconhecido ...');
+}
